18f26fb59ad5be99d6a7a1db235173b05ade4bd7
Justin Yoo <justin.yoojh@gmail.com>
2022-05-18 12:17:46 +0000

    Update Blazor Component (#5)

---
+++ src/frontend/blazor/Models/UserInfo.cs
@@ -0,0 +1,10 @@
+namespace TokenStore.Models
+{
+    public class UserInfo
+    {
+        public virtual string? FirstName { get; set; }
+        public virtual string? LastName { get; set; }
+        public virtual string? Email { get; set; }
+        public virtual string? Phone { get; set; }
+    }
+}
