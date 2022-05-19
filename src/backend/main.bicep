48a2bce4a94761d6d11145f11d84f7d598dda632
Justin Yoo <justin.yoojh@gmail.com>
2022-05-20 00:24:45 +0900

    temp save

--- src/backend/main.bicep
+++ src/backend/main.bicep
@@ -1,9 +1,12 @@
 
 targetScope = 'subscription'
 
-param apim_name string 
 param rg_name string 
-param location string 
+param apim_name string
+param sttapp_name_blazor string
+param sttapp_name_react string
+param depscrpt_name string
+param location string
 
 resource resourcegroup 'Microsoft.Resources/resourceGroups@2021-04-01' = {
   name: rg_name
@@ -15,6 +18,42 @@ module apim './apim.bicep' = {
   name: 'apimservicedeployment'
   params: {
     apim_name: apim_name
+    location: location
+  }
+}
+
+module sttapp_blazor './sttapp.bicep' = {
+  scope: resourcegroup
+  name: 'sttapp_blazor'
+  params: {
+    sttapp_name: sttapp_name_blazor
+    location: 'centralus'
+  }
+}
+
+module sttapp_react './sttapp.bicep' = {
+  scope: resourcegroup
+  name: 'sttapp_react'
+  params: {
+    sttapp_name: sttapp_name_react
+    location: 'centralus'
+  }
+}
+
+module depscrpt './depscrpt.bicep' = {
+  scope: resourcegroup
+  name: 'deploymentscript'
+  dependsOn: [
+    apim
+    sttapp_blazor
+    sttapp_react
+    ]
+  params: {
+    rg_name: rg_name
+    depscrpt_name: depscrpt_name
+    apim_name: apim_name
+    sttapp_name_blazor: sttapp_name_blazor
+    sttapp_name_react: sttapp_name_react
     location: location
   }
 }
---
+++ src/backend/setup-swa.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -e
+
+urls=$(curl -H "Accept: application/vnd.github.v3+json" \
+    https://api.github.com/repos/justinyoo/token-store-demo/releases/latest | \
+    jq '[.assets[] | .browser_download_url]')
+resource_group="rg-$RESOURCE_GROUP_NAME"
+apim_service_name="apim-$APIM_NAME"
+
+subscription_id=$(az account show | jq '.id' -r)
+apim_secret_uri=/subscriptions/$subscription_id/resourceGroups/$resource_group/providers/Microsoft.ApiManagement/service/$apim_service_name/subscriptions/master/listSecrets
+api_version=2021-08-01
+
+subscription_key=$(az rest --method post --uri '$apim_secret_uri?api-version=$api_version' | jq '.primaryKey' -r)
+
+
+for value in `eval echo {0..$suffix_index}`
+do
+    api_zip=$(echo $urls | jq --argjson value $value '.[$value]' -r)
+    fncapp_name="fncapp-$AZ_RESOURCE_NAME-${fncapp_suffixes[$value]}-$AZ_ENVIRONMENT_CODE-$AZ_LOCATION_CODE"
+    fncapp_url="https://$fncapp_name.azurewebsites.net/api/openapi/v3.json"
+
+    # Deploy function apps
+    api_app=$(az functionapp deploy -g $resource_group -n $fncapp_name --src-url $api_zip --type zip)
+
+    api_key=$(az functionapp keys list -g $resource_group -n $fncapp_name --query "functionKeys.default" -o tsv)
+    api_path=$(echo ${fncapp_suffixes[$value]} | tr '-' '/')
+    api_upper_suffix=$(echo ${fncapp_suffixes[$value]} | tr '[a-z]' '[A-Z]' | tr '-' '_')
+    api_name="X_FUNCTIONS_KEY_$api_upper_suffix"
+    api_policy_url="https://raw.githubusercontent.com/devrel-kr/nhn-toast-notification-service-custom-connector/main/infra/apim-api-policy-${fncapp_suffixes[$value]}.xml"
+
+    # Provision APIs to APIM
+    az deployment group create \
+        -n ApiManagement_Api-${fncapp_suffixes[$value]} \
+        -g $resource_group \
+        -u $bicep_url \
+        -p name=$AZ_RESOURCE_NAME \
+        -p env=$AZ_ENVIRONMENT_CODE \
+        -p apiMgmtNameValueName=$api_name \
+        -p apiMgmtNameValueDisplayName=$api_name \
+        -p apiMgmtNameValueValue=$api_key \
+        -p apiMgmtApiName=$api_upper_suffix \
+        -p apiMgmtApiDisplayName=$api_upper_suffix \
+        -p apiMgmtApiDescription=$api_upper_suffix \
+        -p apiMgmtApiPath=$api_path \
+        -p apiMgmtApiFormat="openapi+json-link" \
+        -p apiMgmtApiValue=$fncapp_url \
+        -p apiMgmtApiPolicyFormat="xml-link" \
+        -p apiMgmtApiPolicyValue=$api_policy_url
+
+    # Update hostnames on function apps
+    apim_url="https://apim-$AZ_RESOURCE_NAME-$AZ_ENVIRONMENT_CODE-$AZ_LOCATION_CODE.azure-api.net/$api_path"
+    appsettings_hostnames=$(az functionapp config appsettings list -g $resource_group -n $fncapp_name | jq '.[] | select(.name == "OpenApi__HostNames") | .value' -r)
+    if [ "$appsettings_hostnames" == "" ]
+    then
+        openapi_hostnames=$apim_url
+    else
+        openapi_hostnames=$apim_url,$appsettings_hostnames
+    fi
+
+    appsettings_updated=$(az functionapp config appsettings set -g $resource_group -n $fncapp_name --settings OpenApi__HostNames=$openapi_hostnames)
+
+    # Update NHN Toast endpoints on function apps
+    appsettings=$(curl https://raw.githubusercontent.com/devrel-kr/nhn-toast-notification-service-custom-connector/main/infra/appsettings-endpoints-${fncapp_suffixes[$value]}.json)
+    appsettings_length=$(echo $appsettings | jq '. | length')
+    for (( i=0; i<$appsettings_length; i++ ))
+    do
+        appsettings_name=$(echo $appsettings | jq --arg i "$i" '.[$i|fromjson].name' -r)
+        appsettings_value=$(echo $appsettings | jq --arg i "$i" '.[$i|fromjson].value' -r)
+
+        appsettings_updated=$(az functionapp config appsettings set -g $resource_group -n $fncapp_name --settings $appsettings_name=$appsettings_value)
+    done
+done
